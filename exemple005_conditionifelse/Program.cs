6972fe6e569f5792bb11c69d29e34037f4c66a34
Islam Omarov <adamsmitmity@gmail.com>
2023-09-19 23:46:50 +0300

    add example005 conditionalfelse

---
+++ exemple005_conditionifelse/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("введите имя пользователя ");
+string username = Console.ReadLine();
+
+if(username.ToLower() == "маша")
+{
+    Console.WriteLine("ура, это же Маша");
+}
+else
+{
+Console.Write("привет, ");
+Console.WriteLine(username);
+}
